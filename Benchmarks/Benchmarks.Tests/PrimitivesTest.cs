0a23ecb94dbe4d3e2b5b0fabdf92e836aef9a40c
Angello Astorga <angello.astorga@gmail.com>
2019-02-28 10:04:59 -0600

    adding example

---
+++ Benchmarks/Benchmarks.Tests/PrimitivesTest.cs
@@ -0,0 +1,26 @@
+// <copyright file="PrimitivesTest.cs">Copyright ©  2019</copyright>
+using System;
+using Benchmarks;
+using Microsoft.Pex.Framework;
+using Microsoft.Pex.Framework.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Benchmarks
+{
+    /// <summary>This class contains parameterized unit tests for Primitives</summary>
+    [PexClass(typeof(Primitives))]
+    //[PexAllowedExceptionFromTypeUnderTest(typeof(InvalidOperationException))]
+    //[PexAllowedExceptionFromTypeUnderTest(typeof(ArgumentException), AcceptExceptionSubtypes = true)]
+    [TestClass]
+    public partial class PrimitivesTest
+    {
+        /// <summary>Test stub for Sum(Int32, Int32)</summary>
+        [PexMethod]
+        public int PUT_Sum(int x, int y)
+        {
+            int result = Primitives.Sum(x, y);
+            return result;
+            // TODO: add assertions to method PrimitivesTest.PUT_Sum(Int32, Int32)
+        }
+    }
+}
---
+++ Benchmarks/Benchmarks/Primitives.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Benchmarks
+{
+    public class Primitives
+    {
+        public static int Sum(int x, int y){
+
+            return x + y;
+        }
+    }
+}
